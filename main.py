b671af38b7c03abc965ac1d7e5bb6285163675a7
ricardo.lopes.0133539 <ricardo.lopes.T0133539@edu.atec.pt>
2026-05-05 20:27:52 +0100

    Criação do menu principal

---
+++ main.py
@@ -0,0 +1,36 @@
+while True:
+    print()
+    print("======================")
+    print(" Biblioteca de Filmes")
+    print("======================")
+    print("1. Adcionar Filmes")
+    print("2. Listar Filmes")
+    print("3. Pesquisar Filmes")
+    print("4. Atualizar Filmes")
+    print("5. Remover Filmes")
+    print("6. Ordenar Filmes")
+    print("7. Sair")
+
+    print()
+
+    opcao = input("Escolha uma opção: ")
+
+    match opcao:
+        case "1":
+            print("Adicionar Filmes")
+        case "2":
+            print("Listar Filmes")
+        case "3":
+            print("Pesquisar Filmes")
+        case "4":
+            print("Atualizar Filme")
+        case "5":
+            print("Remover Filmes")
+        case "6":
+            print("Ordenar Filmes")
+        case "7":
+            print("Obrigado, até a próxima.")
+            break
+        case _:
+            print("Opção inválida, deve escolher um número de 1 a 7.")
+            input("Pressione Enter para tentar novamente.")
